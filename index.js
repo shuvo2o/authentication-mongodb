ff94326ed922555ff2e441377e33715f42108054
shuvo2o <eng.shuvo03@gmail.com>
2025-12-09 18:02:32 +0600

    login without jwt

--- index.js
+++ index.js
@@ -59,6 +59,25 @@ async function run() {
             }
             
         })
+
+        // login user
+        app.post("/login", async (req, res) =>{
+            const { email, password} = req.body;
+            try {
+                const userExists = await usersCollection.findOne({ email})
+                console.log(userExists)
+                if (!userExists) return res.status(404).json({ message: "User not found"})
+
+                const isPasswordValid = await bcrypt.compare(password, userExists.password)
+                if (!isPasswordValid) return res.status(401).json({ message: "Invalid password"})
+                res.json({message: "Login succesful"})
+            } catch (error) {
+                 res.status(500).json({
+                    message: "Failed to get user",
+                    error: error.message
+                })
+            }
+        })
         // get all users 
         app.get("/users", async(req, res)=> {
             try {
